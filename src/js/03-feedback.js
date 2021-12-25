1eaee6a1f32f49917fba92f07d943b70117cb73e
Volodymyr Horbatiuk <vladimir.a.gorbatyuk@gmail.com>
2021-12-25 23:48:19 +0200

    debug localStorage

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -4,7 +4,16 @@ const form = document.querySelector('.feedback-form');
 form.addEventListener('input', throttle(onFormData, 500));
 form.addEventListener('submit', onSubmitForm);
 
-const formData = {};
+let formData = {};
+const data = JSON.parse(localStorage.getItem('feedback-form-state'));
+const email = document.querySelector('.feedback-form input');
+const message = document.querySelector('.feedback-form textarea');
+if (data) {
+  email.value = data.email;
+  message.value = data.message;
+  formData = data;
+}
+
 
 function onFormData(e) {
   formData[e.target.name] = e.target.value;
@@ -17,13 +26,3 @@ function onSubmitForm(e) {
   e.currentTarget.reset();
   localStorage.removeItem('feedback-form-state');
 }
-
-(function dataFromLocalStorage() {
-  const data = JSON.parse(localStorage.getItem('feedback-form-state'));
-  const email = document.querySelector('.feedback-form input');
-  const message = document.querySelector('.feedback-form textarea');
-  if (data) {
-    email.value = data.email;
-    message.value = data.message;
-  }
-})();
